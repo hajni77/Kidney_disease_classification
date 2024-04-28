4b83dc38a059cb78471bffdb077abfbc04a4d10b
hajni77 <frecskahajni@gmail.com>
2024-04-28 15:58:49 +0200

    gitignore

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,10 +1,10 @@
 from cnnClassifier.constants import *
 import os
 from cnnClassifier.utils.common import read_yaml, create_directories,save_json
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig,
-                                                TrainingConfig,
-                                                EvaluationConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig)
+                                                # PrepareBaseModelConfig,
+                                                # TrainingConfig,
+                                                # EvaluationConfig)
 
 
 class ConfigurationManager:
